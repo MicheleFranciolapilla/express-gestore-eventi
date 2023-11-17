48f56487213ce1137e7b03f53fbb30ed61e3a08e
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-11-17 23:25:42 +0900

    Trasformazione primo evento da Obj  a Array

--- models/EventModel.js
+++ models/EventModel.js
@@ -1,9 +1,10 @@
 // Importazione librerie
-const   fileSystem  =   require("fs");
-const   path        =   require("path");
+const   fileSystem      =   require("fs");
+const   path            =   require("path");
 
 // Definizione del database (.json)
-const   eventsDB    =   "eventsDB.json";
+const   eventsDB        =   "eventsDB.json";
+const   eventsDBPath    =   path.resolve("public", eventsDB);
 
 // Implementazione classe EventModel
 class   EventModel
@@ -30,9 +31,9 @@ class   EventModel
     constructor(_title, _date, _maxSeats)
     {
         this.#setUniqueId();
-        this.title      =   _title;
-        this.date       =   _date;
-        this.maxSeats   =   _maxSeats;
+        this.#title     =   _title;
+        this.#date      =   _date;
+        this.#maxSeats  =   _maxSeats;
         if (EventModel.addEvent(this.#id, this.#title, this.#date, this.#maxSeats))
             EventModel.eventsInDB++;
         else
@@ -48,7 +49,7 @@ class   EventModel
         // Se il file è corrotto o inesistente lo si ricrea/crea come file vuoto
         if ((errorOnDB == 1) || (errorOnDB == 2))
         {
-            fileSystem.writeFileSync(path.resolve("public", eventsDB), "");
+            fileSystem.writeFileSync(eventsDBPath, "");
             EventModel.someError = 0;
             allEvents = [];
         }
@@ -59,32 +60,45 @@ class   EventModel
     // Metodi statici
     static  checkDBExistance()
     {
-        return fileSystem.existsSync(path.resolve("public", eventsDB));
+        return fileSystem.existsSync(eventsDBPath);
+    }
+
+    static  fromObjToArray(objToArray)
+    {
+        const simpleObj = objToArray;
+        let arrayToReturn = [];
+        arrayToReturn.push(simpleObj);
+        return arrayToReturn;
     }
 
     static  getAllEvents()
             {
-                if (!fileSystem.existsSync(path.resolve("public", eventsDB)))
+                if (!EventModel.checkDBExistance())
                 {
+                    console.log("file non esiste");
                     EventModel.someError = 1;
                     console.log("Errore: ", EventModel.errors[EventModel.someError]);
                     return null;
                 }
                 else
                 {
-                    const fileContent = fileSystem.readFileSync(path.resolve("public", eventsDB), "utf-8");
+                    console.log("file esiste");
+                    const fileContent = fileSystem.readFileSync(eventsDBPath, "utf-8");
                     if (fileContent == "")
                     {
+                        console.log("file senza contenuto.....restituisco array vuoto");
                         EventModel.someError = 0;
                         console.log("Errore: ", EventModel.errors[EventModel.someError]);
                         return [];
                     }
                     else
                     {
+                        console.log("file con contenuto");
                         let jsonFileData = null;
                         try
                         {
                             jsonFileData = JSON.parse(fileContent);
+                            console.log("tento il parsing in formato json....... ", jsonFileData);
                         }
                         catch(error)
                         {
@@ -92,12 +106,27 @@ class   EventModel
                             console.log("Errore: ", EventModel.errors[EventModel.someError]);
                             return null;
                         }
+                        console.log("Nessun errore nel parsing");
+                        if (!Array.isArray(jsonFileData))
+                        {
+                            console.log("jsonfiledata non è un array.... ");
+                            jsonFileData = EventModel.fromObjToArray(jsonFileData);
+                            // const simpleObj = jsonFileData;
+                            // jsonFileData = [];
+                            // jsonFileData = Array.from(jsonFileData);
+                            console.log("jsonfiledata ora è un array.... ", jsonFileData);
+                        }
                         EventModel.someError = 0;
                         console.log("Errore: ", EventModel.errors[EventModel.someError]);
                         return jsonFileData;
                     }
                 }
             } 
+
+    static  getEvent(eventId)
+    {
+
+    }
 
     static  addEvent(_id, _title, _date, _maxSeats)
     {
@@ -109,7 +138,7 @@ class   EventModel
                                 };
         try
         {
-            fileSystem.writeFileSync(path.resolve("public", eventsDB), JSON.stringify(eventToAdd));
+            fileSystem.writeFileSync(eventsDBPath, JSON.stringify(eventToAdd));
         }
         catch (error)
         {

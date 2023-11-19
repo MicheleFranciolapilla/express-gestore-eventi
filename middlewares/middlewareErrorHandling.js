f87b90a4b93512156ba4e483998a5d605efe9f7a
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-11-19 21:00:30 +0100

    Messaggio di errore specifico dal middleware

--- middlewares/middlewareErrorHandling.js
+++ middlewares/middlewareErrorHandling.js
@@ -6,16 +6,16 @@ module.exports  =   function(error, request, response, next)
                                             html:       ()  =>
                                                 {
                                                     response.type("html");
-                                                    response.send("<h1>Errore (500) interno al server (Provvisorio)</h1>");
+                                                    response.send(`<h1>Errore (500) ${error.message}</h1>`);
                                                 },
                                             json:       ()  =>
                                                 {
-                                                    response.json({ "Errore 500"    :   "Errore interno al server (Provvisorio)"});
+                                                    response.json({ "Errore 500"    :   error.message});
                                                 },
                                             default:    ()  =>
                                                 {
                                                     response.type("text");
-                                                    response.send("Errore (500) interno al server (Provvisorio)");
+                                                    response.send(`Errore (500)... ${error.message}`);
                                                 }
                                         });
                     }
